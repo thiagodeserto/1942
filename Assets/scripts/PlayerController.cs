b66f9df09688eec68f6346824565d3d62580645b
Thiago Deserto <thiago@gamepill.com>
2015-06-25 23:30:35 -0300

    Implemented Mouse Controller.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -17,39 +17,78 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private GameObject fire;
 
+    private bool usingMouse = false;
+
+    private Vector3 lastMousePosition;
+
 	// Use this for initialization
 	void Start () {
-	    
+        this.cam = Camera.main;
+        lastMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
 	}
 
     void Fire()
     {
         Instantiate(fire, transform.position, Quaternion.identity);
     }
+
+    private void CheckController(Vector3 mousePosition)
+    {
+        if (mousePosition != lastMousePosition)
+        {
+            this.usingMouse = true;
+        }
+        else if (Input.anyKeyDown)
+        {
+            this.usingMouse = false;
+        }
+        lastMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
 	
 	// Update is called once per frame
     void Update()
     {
-        Vector3 translate = Vector3.zero;
+        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        CheckController(mousePosition);
 
-        if (Input.GetKey("left") && transform.position.x > -0.8f)
+        if(this.usingMouse)
         {
-            translate += Vector3.left * speed * Time.deltaTime;
+            mousePosition.z = transform.position.z;
+            Vector3 direction = Vector3.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+            if (direction.x < -0.8f || direction.x > 0.8f)
+            {
+                direction.x = transform.position.x;
+            }
+            if (direction.y < -1.35f || direction.y > 1.35f)
+            {
+                direction.y = transform.position.y;
+            }
+            transform.position = direction;
         }
-        if (Input.GetKey("right") && transform.position.x < 0.8f)
+        else
         {
-            translate += Vector3.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("up") && transform.position.y < 1.35f)
-        {
-            translate += Vector3.up * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("down") && transform.position.y > -1.35f)
-        {
-            translate += Vector3.down * speed * Time.deltaTime;
+            Vector3 translate = Vector3.zero;
+            if (Input.GetKey("left") && transform.position.x > -0.8f)
+            {
+                translate += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey("right") && transform.position.x < 0.8f)
+            {
+                translate += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey("up") && transform.position.y < 1.35f)
+            {
+                translate += Vector3.up * speed * Time.deltaTime;
+            }
+            if (Input.GetKey("down") && transform.position.y > -1.35f)
+            {
+                translate += Vector3.down * speed * Time.deltaTime;
+            }
+
+            transform.Translate(translate);
         }
 
-        if(Input.GetKey("space"))
+        if(Input.GetKey("space") || Input.GetMouseButton(0))
         {
             if (lastTimeShot + fireFrequency < Time.timeSinceLevelLoad)
             {
@@ -57,7 +96,5 @@ public class PlayerController : MonoBehaviour {
                 Fire();
             }
         }
-
-        transform.Translate(translate);
     }
 }
