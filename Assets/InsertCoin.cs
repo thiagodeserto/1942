8a3097570245fb1c6a5c72b0c16f5a2d61f83d45
Thiago Deserto <thiago@gamepill.com>
2015-06-28 15:19:07 -0300

    Implemented mainmenu. Imported Retro font with a free non commercial license. Adjusted top ui menu to match with retro style and colors.

---
+++ Assets/InsertCoin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InsertCoin : MonoBehaviour {
+
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private float OnTime;
+
+    [SerializeField]
+    private float OffTime;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Blink());
+	}
+	
+    IEnumerator Blink()
+    {
+        while(true)
+        {
+            text.enabled = true;
+            yield return new WaitForSeconds(OnTime);
+            text.enabled = false;
+            yield return new WaitForSeconds(OffTime);
+        }
+    }
+}
